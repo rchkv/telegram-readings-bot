c356cbe10ab2787424722ca64eb951ebd9758e65
rchkv <alas6505@gmail.com>
2018-09-10 18:29:03 +0500

    Изменил на cas
    Кажется, так лучше читается

--- lib/message_responder.rb
+++ lib/message_responder.rb
@@ -4,40 +4,22 @@ require './lib/message_sender'
 class MessageResponder
   attr_reader :message
   attr_reader :bot
-  # attr_reader :user
 
   def initialize(options)
     @bot = options[:bot]
     @message = options[:message]
-    # @user = User.find_or_create_by(uid: message.from.id)
   end
 
   def respond
-    on /^\/start/ do
+    case message.text
+    when '/start'
       answer_with_greeting_message
-    end
-
-    on /^\/stop/ do
+    when '/stop'
       answer_with_farewell_message
     end
   end
 
   private
-
-  def on regex, &block
-    regex =~ message.text
-
-    if $~
-      case block.arity
-      when 0
-        yield
-      when 1
-        yield $1
-      when 2
-        yield $1, $2
-      end
-    end
-  end
 
   def answer_with_greeting_message
     answer_with_message I18n.t('greeting_message')
