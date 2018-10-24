b2b4ec713ba2f9cf411b1bd15d0bd9342b9e45d5
rchkv <alas6505@gmail.com>
2018-10-24 15:23:17 +0500

    Добавил обработку ошибки

--- lib/readings_sender.rb
+++ lib/readings_sender.rb
@@ -50,14 +50,19 @@ class ReadingsSender
   private
 
   def login
-    config = AppConfigurator.new
-    login = config.get_login
-    password = config.get_password
+    begin
+      config = AppConfigurator.new
+      login = config.get_login
+      password = config.get_password
 
-    within('#login_form') do
-      fill_in 'Flogin', with: login
-      fill_in 'Fpass', with: password
-      find('.go').click
+      within('#login_form') do
+        fill_in 'Flogin', with: login
+        fill_in 'Fpass', with: password
+        find('.go').click
+      end
+    rescue Exception => e
+      puts e.message
+      puts e.backtrace.inspect
     end
     page.has_content?('Моя информация')
   end
